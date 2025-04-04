1efb02a0479b32fc776b9f87027dd8066016d3e0
Narmada1821 <nandurinarmada1821@gmail.com>
2025-04-04 14:36:26 +0530

    added 3 git commands

--- sbi.py
+++ sbi.py
@@ -2,3 +2,6 @@ git add – adding a required file (single)
 git add 1
 it status – to check the file status
 git commit -m “commit message” (files available in local repository)
+git log – it shows commit history
+git log --oneline – shows commit history in single line.
+git log --stat – it will show the statastics about the changes
